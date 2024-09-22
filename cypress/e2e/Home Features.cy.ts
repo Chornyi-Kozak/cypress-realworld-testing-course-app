f0ea7c862ee97a17ceacbd575820d17b7a21cec0
BlackKozak <jay.brown@coinpayments.net>
2024-09-22 00:25:17 -0400

    Update

---
+++ cypress/e2e/Home Features.cy.ts
@@ -0,0 +1,3 @@
+it("the features on the homepage are correct", () => {
+  cy.visit("http://localhost:3000")
+})
--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -3,5 +3,24 @@ describe("Newsletter Subscribe Form", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("allows users to subscribe to the email list", () => {})
-})
+  it("allows users to subscribe to the email list", () => {
+    cy.getByData("email-input").type("jaykaybie@hotmail.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message").should("exist").contains("jaykaybie@hotmail.com")
+  })
+
+  it("does NOT allow an invalid email address", () => {
+    cy.getByData("email-input").type("john")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message").should("not.exist")
+  })
+
+  it("does NOT allow already subscribed email address", () => {
+    cy.getByData("email-input").type("john@example.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message")
+    .should("exist")
+    .contains("already exists. Please use a different email address.")
+  })
+  })
+  
